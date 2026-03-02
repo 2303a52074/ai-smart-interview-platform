5f584dbc54ac3575027a03b1a917790401dddf0a
2303a52074 <2303a52074@sru.edu.in>
2026-03-02 23:10:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,5 +30,5 @@ mongoose.connect(process.env.MONGO_URI)
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log("Mongo URI:", process.env.MONGO_URI);;
 });
